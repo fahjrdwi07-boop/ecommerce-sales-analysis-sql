ce6428a44a85a708a672ef54c9701b9367da5e76
fahjrdwi07-boop <fahjrdwi.07@gmail.com>
2026-01-20 08:42:03 +0700

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,26 +1,41 @@
--- Show all customers
+-- Menampilkan semua pelanggan
 SELECT * FROM customers;
 
--- Products with price above 500000
+
+-- Produk dengan harga di atas 500.000
 SELECT product_name, price
 FROM products
 WHERE price > 500000;
 
--- Orders after January 12, 2024
+
+-- Pesanan setelah 12 Januari 2024
 SELECT customer_id, order_date
 FROM orders
 WHERE order_date > '2024-01-12';
 
--- Products sorted by highest price
+
+-- Produk diurutkan dari harga tertinggi
 SELECT product_name, price
 FROM products
 ORDER BY price DESC;
 
--- Total revenue
+
+-- Total omzet
 SELECT SUM(total_amount) AS total_omzet
 FROM orders;
 
--- Total orders per customer
+
+-- Jumlah pesanan tiap pelanggan
 SELECT customer_id, COUNT(*) AS jumlah_pesanan
 FROM orders
 GROUP BY customer_id;
+
+
+-- JOIN: Total belanja per pelanggan
+SELECT c.customer_name,
+COUNT(o.order_id) AS total_orders,
+SUM(o.total_amount) AS total_spent
+FROM customers c
+JOIN orders o ON c.customer_id = o.customer_id
+GROUP BY c.customer_name
+ORDER BY total_spent DESC;
